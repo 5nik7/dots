5a9c31a30f7fe25c283dde7f790a42e43791d170
Nick <91869577+5nik7@users.noreply.github.com>
2023-12-11 19:47:19 -0800

    Update fzf.zsh

--- dotfiles/zsh/fzf.zsh
+++ dotfiles/zsh/fzf.zsh
@@ -4,6 +4,7 @@ then
   export FZF_DEFAULT_OPTS='
     --color fg:-1,bg:-1,hl:5,fg+:3,bg+:-1,hl+:5
     --color info:42,prompt:-1,spinner:42,pointer:51,marker:33
+    --pointer='|>'
     --exact
     --ansi'
   if [ $commands[fd] ]
