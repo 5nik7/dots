363372c5ee7821d5f5953595033d80f309de507e
Nick <91869577+5nik7@users.noreply.github.com>
2023-12-12 17:11:52 -0800

    Update aliases.zsh

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -1,5 +1,3 @@
-#!/usr/bin/zsh
-
 if [ $commands[eza] ]
 then
   alias ll="ll_eza"
@@ -14,22 +12,19 @@ alias q="exit"
 alias ..="cd .."
 alias path='echo $PATH | tr ":" "\n"'
 alias d='ranger'
-alias less='less -R -M -X'
 alias npmup='npm install -g npm@latest'
 alias v="$EDITOR"
 alias vi="$EDITOR"
 alias dot='cd $DOTS'
 alias repos='cd $REPOS'
-# alias cat='bat'
-alias mkdir='mkdir -p'
+alias cat='bat'
+alias mkdir='mkdir -pv'
 
-alias g='git'
 alias lg='lazygit'
 
 alias update='sudo pacman -Syyu'
 
-# alias py='python3'
-# alias python='python3'
-# alias pip='pip3'
+alias py3='python3'
+alias py='python'
 
 # vim:ft=zsh
