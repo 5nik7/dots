626235173f67a0dd85480c88633640beedb1e491
Nick Jensen <nickflip86@gmail.com>
2024-07-10 04:07:37 -0700

    Update @ 2024-07-10 04:07

--- shells/powershell/Scripts/Set-Link.ps1
+++ shells/powershell/Scripts/Set-Link.ps1
@@ -10,58 +10,57 @@ Target directory for the symbolic link.
 #>
 
 param(
-  $base = (Resolve-Path $baseinput).Path,
-  $target = (Resolve-Path $targetinput).Path
+  [Parameter(Mandatory = $true, Position = 0)]
+  [string] $base = (Resolve-Path $base).Path,
+
+  [Parameter(Mandatory = $true, Position = 1)]
+  [string] $target = (Resolve-Path $target).Path
 )
 
-# Check and resolve the base path
-# if (Test-Path $base) {
-#   return
-# }
-# else {
-#   Write-Error "Base path '$base' does not exist."
-#   exit
-# }
-
-$backupDir = "$env:USERPROFILE\backups"
-$arrow = "  -->  "
-$bakDate = Get-Date -Format "yyyy-MM-dd_HH-mm"
-# $basedir = [System.IO.Path]::GetDirectoryName($base)
-$basedir = Split-Path $base -Parent
-$parentDir = Split-Path $basedir -Parent
-$baseleaf = Split-Path -Path $base -Leaf
-$basedircolor = "DarkBlue"
-$targetdir = Split-Path $target -Parent
-$targetleaf = Split-Path -Path $target -Leaf
-$backupFileName = "$targetleaf.$bakDate.bak"
-$targetdircolor = "DarkBlue"
-
-if ($basedir -eq $env:DOTS -or $parentDir -eq $env:DOTFILES) {
-  $basedircolor = "Magenta"
-  $basedir = "   "
-}
-else {
-  $basedir = " $basedir\"
+if (-not (Test-Path -Path $base)) {
+  Write-Host ''
+  Write-Host " $base does not exist." -ForegroundColor Red
+  exit
 }
 
-$targetdir = " $targetdir\"
 function CreateBackup {
-  if (Test-Path -Path $backupDir) {
-    return
-  }
-  else {
-    Write-Host -ForegroundColor Yellow "Creating backup directory: " -NoNewline
+  $backupDir = "$env:USERPROFILE\backups"
+  $bakDate = Get-Date -Format "MM-dd-yyyy-HH-mm"
+  $targetleaf = (Get-Item $target).Name
+  $backupFileName = "$targetleaf.$bakDate.bak"
+  if (!(Test-Path -Path $backupDir)) {
+    Write-Host -ForegroundColor Yellow "  Creating backup directory: " -NoNewline
     New-Item -ItemType Directory -Path $backupDir -ErrorAction Stop | Out-Null
     Write-Host -ForegroundColor White "$backupDir"
   }
   Write-Host ''
-  Write-Host -ForegroundColor Yellow "Creating a backup file: " -NoNewline
+  Write-Host -ForegroundColor Yellow " Backup: " -NoNewline
   Rename-Item -Path $target -NewName $backupFileName -ErrorAction Stop | Out-Null
   Move-Item -Path $backupFileName -Destination $backupDir -ErrorAction Stop | Out-Null
   Write-Host -ForegroundColor White "$backupDir\$backupFileName"
 }
 function CreateLink {
-  New-Item -ItemType SymbolicLink -Path $target -Target $base -ErrorAction Stop | Out-Null
+  New-Item -ItemType SymbolicLink -Path $target -Target ((Get-Item $base).FullName) -ErrorAction Stop | Out-Null
+
+  $arrow = "  -->  "
+  $basepath = [System.IO.Path]::GetFullPath($base)
+  $basedir = [System.IO.Path]::GetDirectoryName($basepath)
+  $parentDir = Split-Path $basepath -Parent
+  $baseleaf = Split-Path -Path $basepath -Leaf
+  $basedircolor = "DarkBlue"
+  $targetpath = [System.IO.Path]::GetFullPath($target)
+  $targetdir = Split-Path $targetpath -Parent
+  $targetleaf = Split-Path -Path $targetpath -Leaf
+  $targetdircolor = "DarkBlue"
+  if ($basedir -eq $env:DOTS -or $parentDir -eq $env:DOTFILES) {
+    $basedircolor = "Magenta"
+    $basedir = "   "
+  }
+  else {
+    $basedir = " $basedir\"
+  }
+
+  $targetdir = " $targetdir\"
   Write-Host ''
   Write-Host -ForegroundColor $basedircolor "$basedir" -NoNewline
   Write-Host -ForegroundColor Cyan "$baseleaf" -NoNewline
@@ -71,25 +70,37 @@ function CreateLink {
   Write-Host ''
 }
 
+if (!(Test-Path -Path $target)) {
+  CreateLink
+  exit
+}
+
 try {
-  if ((Test-Path -Path $target) -and (Get-Item -Path $target).Target -eq $base) {
-    Write-Host -ForegroundColor DarkGray "Already linked."
-  }
-  elseif (Test-Path -Path $target) {
-    $userChoice = Read-Host "The target '$target' already exists. Overwrite? [Y/N]"
-    if ($userChoice -eq 'Y') {
-      CreateBackup
-      CreateLink
-    }
-    else {
-      Write-Host "Operation cancelled by the user."
+  if (Test-Path -Path $target) {
+    if ((Get-Item -Path $target).Target -eq ((Get-Item $base).FullName)) {
+      Write-Host ''
+      Write-Host -ForegroundColor DarkGray " $base and $target already linked."
       exit
     }
-  }
-  else {
-    CreateLink
+    else {
+      Write-Host ''
+      Write-Host " $((Get-Item $target).FullName) " -ForegroundColor Magenta -NoNewline
+      Write-Host "already exists. " -ForegroundColor Gray
+      Write-Host " Create backup file and symlink? " -ForegroundColor Cyan -NoNewline
+      $userChoice = Read-Host "[Y/n]"
+      if (($userChoice -eq 'Y') -or ($userChoice -eq 'y') -or ($userChoice -eq '')) {
+        CreateBackup
+        CreateLink
+      }
+      else {
+        Write-Host ''
+        Write-Host " Operation cancelled." -ForegroundColor Red
+        Write-Host ''
+        exit
+      }
+    }
   }
 }
 catch {
-  Write-Output "Failed to create symbolic link: $_"
+  Write-Output " Failed to create symbolic link: $_"
 }
