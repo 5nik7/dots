b42cf7c1cc438f659f2f41fd8ba85333965f92a9
Nick Jensen <nickflip86@gmail.com>
2024-12-27 19:04:02 -0800

    Update @ 12-27-2024 19:04

--- shells/powershell/Microsoft.VSCode_profile.ps1
+++ shells/powershell/Microsoft.VSCode_profile.ps1
@@ -34,54 +34,54 @@ $wallout = [System.Text.Encoding]::Unicode.GetString($TIC) -replace '(.+)([A-Z]:
 $ENV:WALLPAPER = $wallout
 
 foreach ( $includeFile in ("functions", "aliases", "lab") ) {
-    Unblock-File "$ENV:PSDOT\$includeFile.ps1"
-    . "$ENV:PSDOT\$includeFile.ps1"
+  Unblock-File "$ENV:PSDOT\$includeFile.ps1"
+  . "$ENV:PSDOT\$includeFile.ps1"
 }
 
 $ENV:dotscripts = "$ENV:PSDOT\Scripts"
 if (Test-Path($ENV:dotscripts)) {
-    Add-Path -Path $ENV:dotscripts
+  Add-Path -Path $ENV:dotscripts
 }
 
 $ENV:DOTBIN = "$ENV:DOTS\bin"
 if (Test-Path($ENV:DOTBIN)) {
-    Add-Path -Path $ENV:DOTBIN
+  Add-Path -Path $ENV:DOTBIN
 }
 
 if ($host.Name -eq 'ConsoleHost') {
-    Import-Module PSReadLine
+  Import-Module PSReadLine
 
-    $PSReadLineOptions = @{
-        HistoryNoDuplicates           = $true
-        HistorySearchCursorMovesToEnd = $true
-        HistorySearchCaseSensitive    = $false
-        MaximumHistoryCount           = "50000"
-        ShowToolTips                  = $true
-        ContinuationPrompt            = " "
-        BellStyle                     = "None"
-        PredictionSource              = "History"
-        EditMode                      = "Vi"
-        PredictionViewStyle           = "InlineView"
-        Colors                        = @{
-            Comment                = 'DarkGray'
-            Command                = 'DarkGreen'
-            Emphasis               = 'Cyan'
-            Number                 = 'Yellow'
-            Member                 = 'Blue'
-            Operator               = 'Blue'
-            Type                   = 'Yellow'
-            String                 = 'Green'
-            Variable               = 'Cyan'
-            Parameter              = 'Blue'
-            ContinuationPrompt     = 'Black'
-            Default                = 'White'
-            InlinePrediction       = 'DarkGray'
-            ListPrediction         = 'DarkGray'
-            ListPredictionSelected = 'DarkGray'
-            ListPredictionTooltip  = 'DarkGray'
-        }
+  $PSReadLineOptions = @{
+    HistoryNoDuplicates           = $true
+    HistorySearchCursorMovesToEnd = $true
+    HistorySearchCaseSensitive    = $false
+    MaximumHistoryCount           = "50000"
+    ShowToolTips                  = $true
+    ContinuationPrompt            = " "
+    BellStyle                     = "None"
+    PredictionSource              = "History"
+    EditMode                      = "Vi"
+    PredictionViewStyle           = "InlineView"
+    Colors                        = @{
+      Comment                = 'DarkGray'
+      Command                = 'DarkGreen'
+      Emphasis               = 'Cyan'
+      Number                 = 'Yellow'
+      Member                 = 'Blue'
+      Operator               = 'Blue'
+      Type                   = 'Yellow'
+      String                 = 'Green'
+      Variable               = 'Cyan'
+      Parameter              = 'Blue'
+      ContinuationPrompt     = 'Black'
+      Default                = 'White'
+      InlinePrediction       = 'DarkGray'
+      ListPrediction         = 'DarkGray'
+      ListPredictionSelected = 'DarkGray'
+      ListPredictionTooltip  = 'DarkGray'
     }
-    Set-PSReadLineOption @PSReadLineOptions
+  }
+  Set-PSReadLineOption @PSReadLineOptions
 }
 
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
@@ -93,17 +93,17 @@ Set-PSReadLineKeyHandler -Chord Enter -Function ValidateAndAcceptLine
 Invoke-Expression (&starship init powershell)
 Enable-TransientPrompt
 function Invoke-Starship-TransientFunction {
-    &starship module character
+  &starship module character
 }
 
 Set-PSReadLineOption -ViModeIndicator script -ViModeChangeHandler {
-    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
-    if ($args[0] -eq 'Command') {
-        # Set the cursor to a solid block.
-        Write-Host -NoNewLine "`e[2 q"
-    }
-    else {
-        # Set the cursor to a blinking line.
-        Write-Host -NoNewLine "`e[5 q"
-    }
+  [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+  if ($args[0] -eq 'Command') {
+    # Set the cursor to a solid block.
+    Write-Host -NoNewLine "`e[2 q"
+  }
+  else {
+    # Set the cursor to a blinking line.
+    Write-Host -NoNewLine "`e[5 q"
+  }
 }
