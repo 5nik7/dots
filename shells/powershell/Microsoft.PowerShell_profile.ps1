fb4d5f5959710fdcc36025c9a642cee1c5da7cd5
Nick Jensen <nickflip86@gmail.com>
2024-12-31 19:56:13 -0800

    Update @ 12-31-2024 19:56

--- shells/powershell/Microsoft.PowerShell_profile.ps1
+++ shells/powershell/Microsoft.PowerShell_profile.ps1
@@ -132,3 +132,7 @@ Set-PSReadLineOption -ViModeIndicator script -ViModeChangeHandler {
     Write-Host -NoNewLine "`e[5 q"
   }
 }
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
