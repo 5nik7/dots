e768eeef5c2a0865b6bc689090f61ea238beedcc
njen <contact@njen.io>
2025-01-25 01:58:40 -0800

    Update @ 01-25-2025 01:58

--- shells/powershell/profile.ps1
+++ shells/powershell/profile.ps1
@@ -55,6 +55,7 @@ foreach ( $includeFile in ("functions", "aliases", "lab") ) {
 
 (& gh completion -s powershell) | Out-String | Invoke-Expression
 (& starship completions powershell) | Out-String | Invoke-Expression
+(& bat --completion ps1) | Out-String | Invoke-Expression
 (& tree-sitter complete --shell powershell) | Out-String | Invoke-Expression
 (& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
 (& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
