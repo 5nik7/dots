4dac091127b3e80b42250c1bbe809a9b23db0588
njen <contact@njen.io>
2025-03-20 13:11:19 -0700

    Update @ 03-20-2025 13:11

--- shells/zsh/completions.zsh
+++ shells/zsh/completions.zsh
@@ -1,5 +1,10 @@
-
+# Basic auto/tab complete:
+autoload -U compinit
 zstyle ':completion:*' menu select
+zmodload zsh/complist
+compinit
+_comp_options+=(globdots)   # Include hidden files.
+
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' completer _expand _extensions _complete _ignored _approximate
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
@@ -9,3 +14,4 @@ zstyle ':completion:*' expand prefix suffix
 zstyle ':completion:*' keep-prefix true
 
 zstyle ':completion:*' complete true
+
