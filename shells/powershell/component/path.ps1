7915da36ffe9343bbe8c8913327572e04b268b82
njen <contact@njen.io>
2025-03-21 12:45:44 -0700

    Update @ 03-21-2025 12:45

--- shells/powershell/component/path.ps1
+++ shells/powershell/component/path.ps1
@@ -1,51 +1,59 @@
-function Get-PyenvVersion {
-    $PYVER = (Get-Content "$Env:PYENV\version") -replace '\n', ''
-    return $PYVER
+function Get-PyenvVersion
+{
+  $PYVER = (Get-Content "$Env:PYENV\version") -replace '\n', ''
+  return $PYVER
 }
 $Global:PYVER = Get-PyenvVersion
 $Env:PYVER = $Global:PYVER
 
-function PyenvExePath {
-    $PYENVBIN = (Join-Path $env:PYENV "bin")
-    return $PYENVBIN
+function PyenvExePath
+{
+  $PYENVBIN = (Join-Path $env:PYENV "bin")
+  return $PYENVBIN
 }
 $Global:PYENVBIN = PyenvExePath
 $Env:PYENVBIN = $Global:PYENVBIN
 
-function Get-PyenvExeDir {
-    $PYEXEDIR = (Join-Path "$env:PYENV" "versions\$PYVER")
-    return $PYEXEDIR
+function Get-PyenvExeDir
+{
+  $PYEXEDIR = (Join-Path "$env:PYENV" "versions\$PYVER")
+  return $PYEXEDIR
 }
 $Global:PYEXEDIR = Get-PyenvExeDir
 $Env:PYEXEDIR = $Global:PYEXEDIR
 
-function Get-PyenvScripts {
-    $PYENVSCRIPTS = (Join-Path $PYEXEDIR "Scripts")
-    return $PYENVSCRIPTS
+function Get-PyenvScripts
+{
+  $PYENVSCRIPTS = (Join-Path $PYEXEDIR "Scripts")
+  return $PYENVSCRIPTS
 }
 $Global:PYENVSCRIPTS = Get-PyenvScripts
 $Env:PYENVSCRIPTS = $Global:PYENVSCRIPTS
 
-function Get-PyenvShims {
-    $PYENVSHIMS = (Join-Path $env:PYENV "shims")
-    return $PYENVSHIMS
+function Get-PyenvShims
+{
+  $PYENVSHIMS = (Join-Path $env:PYENV "shims")
+  return $PYENVSHIMS
 }
 $Global:PYENVSHIMS = Get-PyenvShims
 $Env:PYENVSHIMS = $Global:PYENVSHIMS
 
-function Set-Pyenv {
-    @(
-        "$Env:PYENVBIN",
-        "$Env:PYEXEDIR",
-        "$Env:PYENVSCRIPTS",
-        "$Env:PYENVSHIMS"
-    ) | ForEach-Object {
-        $Path = "$_"
-        if ($debug) { Write-Host "$_" }
-        Remove-Path -Path "$Path"
-        Add-Path -Path "$Path"
-        return
+function Set-Pyenv
+{
+  @(
+    "$Env:PYENVBIN",
+    "$Env:PYEXEDIR",
+    "$Env:PYENVSCRIPTS",
+    "$Env:PYENVSHIMS"
+  ) | ForEach-Object {
+    $Path = "$_"
+    if ($debug)
+    { Write-Host "$_" 
     }
+    Remove-Path -Path "$Path"
+    Add-Path -Path "$Path"
+    return
+  }
 }
 
 Set-Pyenv -append
@@ -58,6 +66,7 @@ $env:DOTBIN = "$env:DOTS\bin"
 Add-Path -Path "$env:DOTBIN"
 
 $env:LOCALBIN = "$HOME\.local\bin"
+Remove-Path -Path "$env:LOCALBIN"
 Add-Path -Path "$env:LOCALBIN"
 
 Remove-Path -Path "$Env:LOCALAPPDATA\Microsoft\WindowsApps"
