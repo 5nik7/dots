84200407fb612c1e3a97eaabefb449b04fd71de0
njen <contact@njen.io>
2025-03-21 20:20:59 -0700

    Update @ 03-21-2025 20:20

--- shells/zsh/.zshrc
+++ shells/zsh/.zshrc
@@ -32,7 +32,30 @@ zieces "completions"
 zieces "aliases"
 
 if is_installed fzf; then
-  # zieces "fzf"
+
+  if cmd_exists fd; then
+      export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --exclude .git'
+  fi
+
+  # if cmd_exists bat; then
+  #      CAT_PREVIEWER='bat --style=numbers --color=always --pager=never'
+  #    else
+  #      CAT_PREVIEWER='cat'
+  # fi
+  #
+  export FZF_DEFAULT_OPTS="--height 80% \
+    --border sharp \
+    --layout reverse \
+    --info right \
+    --prompt '> ' \
+    --pointer '┃' \
+    --marker '│' \
+    --separator '──' \
+    --scrollbar '│' \
+    --preview-window='border-sharp' \
+    --preview-window='right:65'"
+
+  export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color bg+:0,bg:-1,spinner:4,hl:7:underline,fg:8,header:3,info:8,pointer:6,marker:14,fg+:6,prompt:2,hl+:10:underline,gutter:-1,selected-bg:0,separator:0,preview-border:8,border:8,preview-bg:-1,preview-label:0,label:7,query:13,input-border:4"
 
   function finst() {
     fzpkgs="$(pkg list-all | tr '/' ' '  | grep -v installed | grep -v Listing | awk '{print $1}' | fzf --preview 'apt-cache show {}')"
--- shells/zsh/fzf.zsh
+++ shells/zsh/fzf.zsh
@@ -2,46 +2,24 @@ if cmd_exists fd; then
   export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --exclude .git'
 fi
 
-# if cmd_exists bat; then
-#    CAT_PREVIEWER='bat --style=numbers --color=always --pager=never'
-# else
-#    CAT_PREVIEWER='cat'
-# fi
-
-FZF_COLORS="bg+:0,\
-bg:-1,\
-spinner:4,\
-hl:7:underline,\
-fg:8,\
-header:3,\
-info:8,\
-pointer:6,\
-marker:14,\
-fg+:6,\
-prompt:2,\
-hl+:10:underline,\
-gutter:-1,\
-selected-bg:0,\
-separator:0,\
-preview-border:8,\
-border:8,\
-preview-bg:-1,\
-preview-label:0,\
-label:7,\
-query:13,\
-input-border:4"
+if cmd_exists bat; then
+   CAT_PREVIEWER='bat --style=numbers --color=always --pager=never'
+else
+   CAT_PREVIEWER='cat'
+fi
 
 export FZF_DEFAULT_OPTS="--height 80% \
 --border sharp \
 --layout reverse \
 --info right \
---color '$FZF_COLORS' \
 --prompt '> ' \
 --pointer '┃' \
 --marker '│' \
 --separator '──' \
 --scrollbar '│' \
 --preview-window='border-sharp' \
---preview-window='right:65%'"
-# --preview '$CAT_PREVIEWER {}'"
+--preview-window='right:65%'
+--preview '$CAT_PREVIEWER {}'"
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color bg+:0,bg:-1,spinner:4,hl:7:underline,fg:8,header:3,info:8,pointer:6,marker:14,fg+:6,prompt:2,hl+:10:underline,gutter:-1,selected-bg:0,separator:0,preview-border:8,border:8,preview-bg:-1,preview-label:0,label:7,query:13,input-border:4"
 
