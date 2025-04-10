e030c6aa3ab17ac6eeecac56a59f3136fdb37859
njen <contact@njen.io>
2025-04-10 13:29:38 -0700

    Update @ 04-10-2025 13:29

--- configs/yazi/init.lua
+++ configs/yazi/init.lua
@@ -1,21 +1,20 @@
-require("full-border"):setup()
+-- require("full-border"):setup()
 
-THEME.git = THEME.git or {}
-THEME.git.modified = ui.Style():fg("blue")
-THEME.git.deleted = ui.Style():fg("red"):bold()
+-- THEME.git = THEME.git or {}
+-- THEME.git.modified = ui.Style():fg("blue")
+-- THEME.git.deleted = ui.Style():fg("red"):bold()
 
-require("git"):setup()
+-- require("git"):setup()
 
-function Linemode:size_and_mtime()
-	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then
-		time = ""
-	elseif os.date("%Y", time) == os.date("%Y") then
-		time = os.date("%b %d %H:%M", time)
-	else
-		time = os.date("%b %d  %Y", time)
-	end
-
-	local size = self._file:size()
-	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
-end
+--   local time = math.floor(self._file.cha.mtime or 0)
+--   if time == 0 then
+--     time = ""
+--   elseif os.date("%Y", time) == os.date("%Y") then
+--     time = os.date("%b %d %H:%M", time)
+--   else
+--     time = os.date("%b %d  %Y", time)
+--   end
+--
+--   local size = self._file:size()
+--   return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+-- end
