d0ee13282eca1354c3d0b2f489c58fd86c47ccc8
5nik7 <nickflip86@gmail.com>
2025-04-22 05:31:30 -0700

    Update @ 04-22-2025 05:31

---
+++ shells/powershell/Scripts/write-date.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+	Writes the current date
+.DESCRIPTION
+	This PowerShell script determines and writes the current date.
+.EXAMPLE
+	PS> ./write-date
+	󰸗 12/25/2022
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+  [system.threading.thread]::currentthread.currentculture = [system.globalization.cultureinfo]'en-US'
+  $CurrentDate = (Get-Date).ToShortDateString()
+  "󰸗 $CurrentDate"
+  exit 0 # success
+}
+catch {
+  " Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+  exit 1
+}
---
+++ shells/powershell/Scripts/write-time.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+	Writes the current time
+.DESCRIPTION
+	This PowerShell script queries the current time and writes it to the console.
+.EXAMPLE
+	PS> ./write-time.ps1
+	 7:20 PM
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+  [system.threading.thread]::currentThread.currentCulture = [system.globalization.cultureInfo]'en-US'
+  $CurrentTime = $((Get-Date).ToShortTimeString())
+  " $CurrentTime"
+  exit 0 # success
+}
+catch {
+  " Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+  exit 1
+}
