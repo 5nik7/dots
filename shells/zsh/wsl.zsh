f950eb81b659c32ed767b72885be70f75481dfdc
5nik7 <nickflip86@gmail.com>
2025-05-23 01:45:14 -0700

    meh

--- shells/zsh/wsl.zsh
+++ shells/zsh/wsl.zsh
@@ -1,6 +1,6 @@
-extend_path "$WINHOME/scoop/shims/"
+extend_path "$WINHOME/scoop/shims"
 extend_path "$WINROOT/shims"
 extend_path "$WINROOT/vscode/bin"
 extend_path "$WINDIR/System32/WindowsPowerShell/v1.0"
 extend_path "$WINDIR/System32"
-extend_path "$WINDER"
+extend_path "$WINDIR"
