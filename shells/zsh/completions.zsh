16b164b5203a89de407fadea0eabb94308d77ecc
5nik7 <nickflip86@gmail.com>
2025-06-13 09:42:16 -0700

    Update @ 06-13-2025 09:42

--- shells/zsh/completions.zsh
+++ shells/zsh/completions.zsh
@@ -27,11 +27,9 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 zstyle ':completion:*' menu no
 
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --icons -1 --color=always $realpath'
 
-zstyle ':fzf-tab:*' fzf-flags --style=full --height=~90% --border=sharp --input-border=sharp --list-border=sharp --color=bg+:0,bg:-1,preview-bg:-1,selected-bg:0,fg:7,fg+:6,hl:7:underline,hl+:10:underline,header:3,info:8,query:13,gutter:-1,pointer:6,marker:14,prompt:2,spinner:4,label:7,preview-label:0,separator:0,border:0,list-border:0,preview-border:0,input-border:8
-
-# zstyle ':fzf-tab:*' use-fzf-default-opts yes
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
 
 # switch group using `<` and `>`
 zstyle ':fzf-tab:*' switch-group '<' '>'
