39f75a176af0b4ebd474d604f221dffbb76dd9fd
5nik7 <nickflip86@gmail.com>
2025-06-18 13:41:49 -0700

    yaz

--- configs/yazi/init.lua
+++ configs/yazi/init.lua
@@ -1,4 +1,6 @@
 require("full-border"):setup()
+require("git"):setup()
+require("folder-rules"):setup()
 
 local catppuccin_theme = require("yatline-catppuccin"):setup("mocha")
 
@@ -25,24 +27,29 @@ require("yatline"):setup({
             },
             section_b = {},
             section_c = {
+                { type = "coloreds", custom = false, name = "symlink" },
+
                 -- {
                 --     type = "string",
                 --     custom = false,
                 --     name = "hovered_path",
                 --     params = { { trimed = false, max_length = 24, trim_length = 10 } },
                 -- },
-                { type = "coloreds", custom = false, name = "tab_path" },
-                { type = "string", custom = false, name = "hovered_name", params = { { trimed = false, show_symlink = true, max_length = 24, trim_length = 10 } },
-                },
+                -- { type = "coloreds", custom = false, name = "tab_path" },
+                -- {
+                --     type = "string",
+                --     custom = false,
+                --     name = "hovered_name",
+                --     params = { { trimed = false, show_symlink = true, max_length = 24, trim_length = 10 } },
+                -- },
             },
         },
         right = {
             section_a = {
-                { type = "string", custom = false, name = "date", params = { "%I:%M" } },
-
+                -- { type = "string", custom = false, name = "date", params = { "%I:%M" } },
             },
             section_b = {
-                { type = "string", custom = false, name = "date", params = { "%a %b %d" } },
+                -- { type = "string", custom = false, name = "date", params = { "%a %b %d" } },
             },
             section_c = {
                 { type = "coloreds", custom = false, name = "githead" },
@@ -65,20 +72,61 @@ require("yatline"):setup({
         },
         right = {
             section_a = {
-                { type = "string", custom = false, name = "cursor_position" },
+                { type = "string", custom = false, name = "date", params = { "%I:%M %p" } },
+                -- { type = "string", custom = false, name = "cursor_position" },
             },
             section_b = {
-                { type = "string", custom = false, name = "cursor_percentage" },
+                -- { type = "string", custom = false, name = "cursor_percentage" },
             },
             section_c = {
-                { type = "string",   custom = false, name = "hovered_file_extension", params = { true } },
+                { type = "string", custom = false, name = "hovered_file_extension", params = { true } },
                 { type = "coloreds", custom = false, name = "permissions" },
             },
         },
     },
 })
+require("yatline-symlink"):setup({
+    symlink_color = "cyan",
+})
 
-require("yatline-githead"):setup()
+require("yatline-githead"):setup({
+    show_branch = true,
+    branch_prefix = "",
+    prefix_color = "white",
+    branch_color = "bright black",
+    branch_symbol = "",
+    branch_borders = "",
+
+    commit_color = "bright magenta",
+    commit_symbol = "@",
+
+    show_behind_ahead = true,
+    behind_color = "bright magenta",
+    behind_symbol = "⇣",
+    ahead_color = "bright magenta",
+    ahead_symbol = "⇡",
+
+    show_stashes = true,
+    stashes_color = "bright magenta",
+    stashes_symbol = "$",
+
+    show_state = true,
+    show_state_prefix = true,
+    state_color = "red",
+    state_symbol = "~",
+
+    show_staged = true,
+    staged_color = "bright yellow",
+    staged_symbol = "+",
+
+    show_unstaged = true,
+    unstaged_color = "bright yellow",
+    unstaged_symbol = "!",
+
+    show_untracked = true,
+    untracked_color = "blue",
+    untracked_symbol = "?",
+})
 
 require("yatline-tab-path"):setup({
     path_fg = "cyan",
@@ -89,3 +137,7 @@ require("yatline-tab-path"):setup({
     flatten_label = " flatten",
     separator = "  ",
 })
+
+th.git = th.git or {}
+th.git.modified = ui.Style():fg("yellow")
+th.git.deleted = ui.Style():fg("red"):bold()
