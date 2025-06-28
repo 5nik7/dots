9f03adacc8e5876c0c1c4623ecf73f38915ff053
5nik7 <nickflip86@gmail.com>
2025-06-27 17:40:06 -0700

    Update @ 06-27-2025 17:40

--- shells/zsh/fzf.zsh
+++ shells/zsh/fzf.zsh
@@ -36,6 +36,20 @@ if cmd_exists fd; then
   export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --exclude .git'
 fi
 
+export _PREVIEW_="$HOME/dots/shells/zsh/preview.zsh"
+local extract="
+local in=\${\${\"\$(<{f})\"%\$'\0'*}#*\$'\0'}
+local -A ctxt
+for entry in \${(@ps:\2:)CTXT}; do
+      local key=\${entry%%=*}
+      local value=\${entry#*=}
+      ctxt[\$key]=\$value
+done
+local realpath=\${ctxt[IPREFIX]}\${ctxt[hpre]}\$in
+realpath=\${(Qe)~realpath}
+"
+zstyle ':fzf-tab:complete:*:*' fzf-flags --preview=$extract';$_PREVIEW_ $realpath'
+
 # --preview-label=' PREVIEW ' \
 # --border-label=' FILES ' \
 export FZF_DEFAULT_OPTS="--style minimal \
---
+++ shells/zsh/preview.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+file="$(realpath "$1")"
+# Check if the file exists
+if [[ -e "$file" ]]; then
+  if [[ $(file --mime-type -b "$file") == text/* ]]; then
+      # Use highlight for syntax highlighting or fallback to cat
+      (highlight -O ansi "$file" || bat --style="${BAT_STYLE:-plain}" --color=always --pager=never "$file") 2> /dev/null | head -500
+    elif  [[ $(file --mime-type -b "$file") == */directory ]]; then
+      eza -a -l --group-directories-first --git-repos --git --icons --time-style relative --no-permissions --no-filesize --no-time --no-user --hyperlink --follow-symlinks --no-quotes --color=always "$file" | head -500
+   else
+    # If not a text file, display the file type
+    file "$file"
+  fi
+else
+  # If the file does not exist, print the filename
+  echo "$1"
+fi
+
