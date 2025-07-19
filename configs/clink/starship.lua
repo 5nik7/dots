2da717fdb3b0033989192d368b12bc90c4c1db2d
5nik7 <nickflip86@gmail.com>
2025-07-19 06:58:30 -0700

    trim

--- configs/clink/starship.lua
+++
@@ -1,5 +0,0 @@
-os.setenv("STARSHIP_CONFIG", "C:\\Users\\njen\\dots\\configs\\starship\\starship.toml")
-
-os.setenv("STARSHIP_CACHE", "C:\\Users\\njen\\AppData\\Local\\Temp")
-
-load(io.popen("starship init cmd"):read("*a"))()
