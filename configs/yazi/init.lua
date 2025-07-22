bb9a7b8fdb07e44e88d96a30b3853c5087b6364e
5nik7 <nickflip86@gmail.com>
2025-07-21 18:18:01 -0700

    Update @ 07-21-2025 18:17

--- configs/yazi/init.lua
+++ configs/yazi/init.lua
@@ -1,4 +1,8 @@
-require("full-border"):setup()
+require("full-border"):setup {
+  -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+  type = ui.Border.PLAIN,
+}
+
 require("git"):setup()
 require("folder-rules"):setup()
 
