bf15354c245f31bb2041decb75a6a5acd8823347
5nik7 <nickflip86@gmail.com>
2025-10-18 02:26:33 -0700

    wal

--- shells/zsh/wsl.zsh
+++ shells/zsh/wsl.zsh
@@ -4,6 +4,7 @@ export WINDIR=$WINROOT/Windows
 
 extend_path "$WINHOME/scoop/shims"
 extend_path "$WINROOT/shims"
+extend_path "$WINROOT/shims/pwsh"
 extend_path "$WINROOT/vscode/bin"
 extend_path "$WINDIR/System32/WindowsPowerShell/v1.0"
 extend_path "$WINDIR/System32"
