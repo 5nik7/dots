9c55b5b6b1ed4c70ab9c8e09a3e42b61d38ad4c4
5nik7 <nickflip86@gmail.com>
2025-10-21 12:53:05 -0700

    Update @ 10-21-2025 12:53

--- bin/git_before_remote.sh
+++ bin/git_before_remote.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 GIT_BEFORE_REPO=$(command dirname $(git rev-parse --show-toplevel))
 if [[ "$GIT_BEFORE_REPO" =~ "/mnt/c/Users/$USERNAME" ]]; then
   GIT_BEFORE_REPO=$(echo "$GIT_BEFORE_REPO" | sed "s|^/mnt/c/Users/$USERNAME|~|")
