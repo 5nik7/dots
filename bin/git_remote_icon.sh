6c1f5d842740c9ce4556d09982ddb1c03d2cb57b
5nik7 <nickflip86@gmail.com>
2025-11-12 09:36:00 -0800

    Update @ 11-12-2025 09:35

--- bin/git_remote_icon.sh
+++ bin/git_remote_icon.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-GIT_REMOTE=$(command git remote get-url origin 2> /dev/null)
+GIT_REMOTE=$(command git remote get-url origin 2>/dev/null)
 if [[ -z "$GIT_REMOTE" ]]; then
-GIT_REMOTE=$(command git ls-remote --get-url 2> /dev/null)
+  GIT_REMOTE=$(command git ls-remote --get-url 2>/dev/null)
 fi
 if [[ "$GIT_REMOTE" =~ "github" ]]; then
-    GIT_REMOTE_SYMBOL=" "
+  # GIT_REMOTE_SYMBOL=""
+  GIT_REMOTE_SYMBOL=""
 elif [[ "$GIT_REMOTE" =~ "gitlab" ]]; then
-    GIT_REMOTE_SYMBOL=" "
+  GIT_REMOTE_SYMBOL=""
 elif [[ "$GIT_REMOTE" =~ "bitbucket" ]]; then
-    GIT_REMOTE_SYMBOL=" "
+  GIT_REMOTE_SYMBOL=""
 elif [[ -n "$GIT_REMOTE" ]]; then
-    GIT_REMOTE_SYMBOL=" "
+  GIT_REMOTE_SYMBOL=""
 else
-    GIT_REMOTE_SYMBOL=" "
+  GIT_REMOTE_SYMBOL=""
 fi
 echo "$GIT_REMOTE_SYMBOL"
