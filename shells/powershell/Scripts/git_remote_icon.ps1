985cd5c9288de7bc357108209902bbba3cf1697e
5nik7 <nickflip86@gmail.com>
2025-11-12 12:12:07 -0800

    Update @ 11-12-2025 12:12

--- shells/powershell/Scripts/git_remote_icon.ps1
+++ shells/powershell/Scripts/git_remote_icon.ps1
@@ -1,5 +1,5 @@
 $gitRemote = git ls-remote --get-url
-$symbol = ' ' # If no Git Remote is returned.
+$symbol = '' # If no Git Remote is returned.
 switch -regex ($gitRemote)
 {
   'github' { $symbol = '' }
