a1b7bc8bae8960fd9334417797a150c28db983ce
njen <contact@njen.io>
2026-04-14 00:37:37 -0700

    Update @ 04-14-2026 00:37

--- shells/zsh/functions.zsh
+++ shells/zsh/functions.zsh
@@ -43,6 +43,7 @@ function gup() {
       if [[ "$root" != "$cwd" ]]; then
         cd "$root"
       fi
+      git pull --recurse-submodules -q
       for subdir in "${subdirs[@]}"; do
         if [[ -d "$subdir" ]]; then
           cd "$subdir"
