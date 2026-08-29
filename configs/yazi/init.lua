3a9afcdf00f68cf0d805f71c44722a558113c6b8
njen <contact@njen.io>
2026-08-28 18:23:55 -0700

    Sync via termux on 2026-08-28 18:23

--- configs/yazi/init.lua
+++ configs/yazi/init.lua
@@ -92,191 +92,191 @@ require("projects"):setup({
   },
 })
 
--- require("yatline"):setup({
+require("yatline"):setup({
+
+  section_separator = { open = "", close = "" },
+  part_separator = { open = "", close = "" },
+  inverse_separator = { open = "", close = "" },
+
+  padding = { inner = 1, outer = 1 },
+
+  style_a = {
+    fg = palette.crust,
+    bold = false,
+    underline = false,
+    reversed = true,
+    bg_mode = {
+      normal = palette.blue,
+      select = palette.mauve,
+      un_set = palette.red,
+    },
+  },
+  style_b = { bg = palette.surface0, fg = palette.text },
+  style_c = { bg = palette.base, fg = palette.text },
+
+  permissions_t_fg = palette.subtext1,
+  permissions_r_fg = palette.yellow,
+  permissions_w_fg = palette.red,
+  permissions_x_fg = palette.green,
+  permissions_s_fg = palette.surface1,
+
+  tab_width = 0,
+
+  selected = { icon = "󰻭", fg = "yellow" },
+  copied = { icon = "", fg = "green" },
+  cut = { icon = "", fg = "red" },
+
+  files = { icon = "", fg = "blue" },
+  filtereds = { icon = "", fg = "magenta" },
+
+  total = { icon = "󰮍", fg = "yellow" },
+  success = { icon = "", fg = "green" },
+  failed = { icon = "", fg = "red" },
+
+  show_background = true,
+
+  display_header_line = true,
+  display_status_line = true,
+
+  component_positions = { "header", "tab", "status" },
+
+  header_line = {
+    left = {
+      section_a = {
+        { type = "line", name = "tabs" },
+      },
+      section_b = {
+        {
+          type = "coloreds",
+          custom = false,
+          name = "string_based_component",
+          params = { "tab_path", "blue", { false, 24, 10 } },
+        },
+        -- { type = "string", custom = false, name = "tab_path", params = { false, 24, 10 } },
+        -- { type = "coloreds", custom = false, name = "tab_path" },
+      },
+      section_c = {
+
+        { type = "string", custom = false, name = "hovered_name" },
+      },
+    },
+    right = {
+      section_a = {
+        -- { type = "string", custom = false, name = "tab_num_files" },
+        -- { type = "string", name = "date", params = { "%A, %d %B %Y" } },
+      },
+      section_b = {
+        -- { type = "string", name = "date", params = { "%X" } },
+      },
+      section_c = {},
+    },
+  },
+
+  status_line = {
+    left = {
+      section_a = {
+        { type = "string", name = "tab_mode" },
+      },
+      section_b = {
+        -- { type = "string", name = "hovered_size" },
+      },
+      section_c = {
+        { type = "coloreds", custom = false, name = "githead" },
+        -- { type = "coloreds", name = "count" },
+      },
+    },
+    right = {
+      section_a = {
+        -- { type = "string", name = "cursor_position" },
+        -- { type = "string", name = "hovered_size" },
+      },
+      section_b = {
+        -- { type = "string", name = "cursor_percentage" },
+        -- { type = "coloreds", custom = false, name = "permissions" },
+      },
+      section_c = {
+        { type = "coloreds", name = "permissions" },
+        -- { type = "string", name = "hovered_file_extension", params = { true } },
+        -- { type = "coloreds", name = "permissions" },
+      },
+    },
+  },
+})
 --
---   section_separator = { open = "", close = "" },
---   part_separator = { open = "", close = "" },
---   inverse_separator = { open = "", close = "" },
---
---   padding = { inner = 1, outer = 1 },
---
---   style_a = {
---     fg = palette.crust,
---     bold = false,
---     underline = false,
---     reversed = true,
---     bg_mode = {
---       normal = palette.blue,
---       select = palette.mauve,
---       un_set = palette.red,
---     },
---   },
---   style_b = { bg = palette.surface0, fg = palette.text },
---   style_c = { bg = palette.base, fg = palette.text },
---
---   permissions_t_fg = palette.subtext1,
---   permissions_r_fg = palette.yellow,
---   permissions_w_fg = palette.red,
---   permissions_x_fg = palette.green,
---   permissions_s_fg = palette.surface1,
---
---   tab_width = 0,
---
---   selected = { icon = "󰻭", fg = "yellow" },
---   copied = { icon = "", fg = "green" },
---   cut = { icon = "", fg = "red" },
---
---   files = { icon = "", fg = "blue" },
---   filtereds = { icon = "", fg = "magenta" },
---
---   total = { icon = "󰮍", fg = "yellow" },
---   success = { icon = "", fg = "green" },
---   failed = { icon = "", fg = "red" },
---
---   show_background = true,
---
---   display_header_line = true,
---   display_status_line = true,
---
---   component_positions = { "header", "tab", "status" },
---
---   header_line = {
---     left = {
---       section_a = {
---         { type = "line", name = "tabs" },
---       },
---       section_b = {
---         {
---           type = "coloreds",
---           custom = false,
---           name = "string_based_component",
---           params = { "tab_path", "blue", { false, 24, 10 } },
---         },
---         -- { type = "string", custom = false, name = "tab_path", params = { false, 24, 10 } },
---         -- { type = "coloreds", custom = false, name = "tab_path" },
---       },
---       section_c = {
---
---         { type = "string", custom = false, name = "hovered_name" },
---       },
---     },
---     right = {
---       section_a = {
---         -- { type = "string", custom = false, name = "tab_num_files" },
---         -- { type = "string", name = "date", params = { "%A, %d %B %Y" } },
---       },
---       section_b = {
---         -- { type = "string", name = "date", params = { "%X" } },
---       },
---       section_c = {},
---     },
---   },
---
---   status_line = {
---     left = {
---       section_a = {
---         { type = "string", name = "tab_mode" },
---       },
---       section_b = {
---         -- { type = "string", name = "hovered_size" },
---       },
---       section_c = {
---         { type = "coloreds", custom = false, name = "githead" },
---         -- { type = "coloreds", name = "count" },
---       },
---     },
---     right = {
---       section_a = {
---         -- { type = "string", name = "cursor_position" },
---         -- { type = "string", name = "hovered_size" },
---       },
---       section_b = {
---         -- { type = "string", name = "cursor_percentage" },
---         -- { type = "coloreds", custom = false, name = "permissions" },
---       },
---       section_c = {
---         { type = "coloreds", name = "permissions" },
---         -- { type = "string", name = "hovered_file_extension", params = { true } },
---         -- { type = "coloreds", name = "permissions" },
---       },
---     },
---   },
--- })
---
--- require("yatline-githead"):setup({
---   order = {
---     "branch",
---     "remote",
---     "tag",
---     "commit",
---     "behind_ahead_remote",
---     "stashes",
---     "state",
---     "staged",
---     "unstaged",
---     "untracked",
---   },
---
---   show_numbers = true, -- shows staged, unstaged, untracked, stashes count
---
---   show_branch = true,
---   branch_prefix = "",
---   branch_color = palette.mauve,
---   branch_symbol = "",
---   branch_borders = "",
---
---   show_remote_branch = true, -- only shown if different from local branch
---   always_show_remote_branch = false, -- always show remote branch even if it the same as local branch
---   always_show_remote_repo = false, -- Adds `origin/` if `always_show_remote_branch` is enabled
---   remote_branch_prefix = ":",
---   remote_branch_color = "bright magenta",
---
---   show_tag = true, -- only shown if branch is not available
---   always_show_tag = false,
---   tag_color = "magenta",
---   tag_symbol = "#",
---
---   show_commit = true, -- only shown if branch AND tag are not available
---   always_show_commit = false,
---   commit_color = "bright magenta",
---   commit_symbol = "@",
---
---   show_behind_ahead_remote = true,
---   behind_remote_color = palette.pink,
---   behind_remote_symbol = "⇣",
---   ahead_remote_color = palette.pink,
---   ahead_remote_symbol = "⇡",
---
---   show_stashes = true,
---   stashes_color = palette.overlay2,
---   stashes_symbol = "$",
---
---   show_state = true,
---   show_state_prefix = true,
---   state_color = palette.red,
---   state_symbol = "~",
---
---   show_staged = true,
---   staged_color = palette.teal,
---   staged_symbol = "+",
---
---   show_unstaged = true,
---   unstaged_color = palette.sapphire,
---   unstaged_symbol = "!",
---
---   show_untracked = true,
---   untracked_color = palette.teal,
---   untracked_symbol = "?",
--- })
--- Status:children_add(function(self)
---   local arrow = "  "
---   local h = self._current.hovered
---   if h and h.link_to then
---     return ui.Line({
---       ui.Span(tostring(arrow)):fg("darkgray"),
---       ui.Span(tostring(h.link_to)):fg(palette.sapphire),
---     })
---   else
---     return ""
---   end
--- end, 3300, Status.LEFT)
+require("yatline-githead"):setup({
+  order = {
+    "branch",
+    "remote",
+    "tag",
+    "commit",
+    "behind_ahead_remote",
+    "stashes",
+    "state",
+    "staged",
+    "unstaged",
+    "untracked",
+  },
+
+  show_numbers = true, -- shows staged, unstaged, untracked, stashes count
+
+  show_branch = true,
+  branch_prefix = "",
+  branch_color = palette.mauve,
+  branch_symbol = "",
+  branch_borders = "",
+
+  show_remote_branch = true, -- only shown if different from local branch
+  always_show_remote_branch = false, -- always show remote branch even if it the same as local branch
+  always_show_remote_repo = false, -- Adds `origin/` if `always_show_remote_branch` is enabled
+  remote_branch_prefix = ":",
+  remote_branch_color = "bright magenta",
+
+  show_tag = true, -- only shown if branch is not available
+  always_show_tag = false,
+  tag_color = "magenta",
+  tag_symbol = "#",
+
+  show_commit = true, -- only shown if branch AND tag are not available
+  always_show_commit = false,
+  commit_color = "bright magenta",
+  commit_symbol = "@",
+
+  show_behind_ahead_remote = true,
+  behind_remote_color = palette.pink,
+  behind_remote_symbol = "⇣",
+  ahead_remote_color = palette.pink,
+  ahead_remote_symbol = "⇡",
+
+  show_stashes = true,
+  stashes_color = palette.overlay2,
+  stashes_symbol = "$",
+
+  show_state = true,
+  show_state_prefix = true,
+  state_color = palette.red,
+  state_symbol = "~",
+
+  show_staged = true,
+  staged_color = palette.teal,
+  staged_symbol = "+",
+
+  show_unstaged = true,
+  unstaged_color = palette.sapphire,
+  unstaged_symbol = "!",
+
+  show_untracked = true,
+  untracked_color = palette.teal,
+  untracked_symbol = "?",
+})
+Status:children_add(function(self)
+  local arrow = "  "
+  local h = self._current.hovered
+  if h and h.link_to then
+    return ui.Line({
+      ui.Span(tostring(arrow)):fg("darkgray"),
+      ui.Span(tostring(h.link_to)):fg(palette.sapphire),
+    })
+  else
+    return ""
+  end
+end, 3300, Status.LEFT)
